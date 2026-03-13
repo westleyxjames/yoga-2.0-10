0e2ef82f18cc2ae887369e5c951349c0f0e70092
westleyxjames <westleyjames7899@gmail.com>
2026-03-13 15:42:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+     <!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,5014226,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5014226&101" alt="counter stats" border="0"></a></noscript>
+<!-- Histats.com  END  -->
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="ZenNest Yoga - Find balance, strength, and inner peace through yoga. Beginner-friendly classes, experienced instructors, and a welcoming community.">
